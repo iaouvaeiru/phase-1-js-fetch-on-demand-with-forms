0a2c8ccc85ede8566ffbc98a7374043b65a0e9ee
Leon Fan <leonkind@gmail.com>
2021-05-05 14:32:11 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = e.target.searchByID.value
+
+    fetch(`http://localhost:3000/movies/${userInput}`)
+    .then( res => res.json())
+    .then( stuff => {
+        appendMovieInfo(stuff)
+    })
+
+
+
+  })
+}
+
+const appendMovieInfo = (input) => {
+    let movieName = document.querySelector('h4')
+    let movieDetails = document.querySelector('p')
+    movieName.innerHTML = input.title
+    movieDetails.innerHTML = input.summary
 }
 
 document.addEventListener('DOMContentLoaded', init);
